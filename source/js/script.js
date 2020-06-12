120e319b279c8f8a73b6c9defb6e1d924437d57f
Vadim Chapovsky <vad.hdtv@gmail.com>
2020-06-12 19:29:15 +0300

    Add animations for module2-task2

--- source/js/script.js
+++ source/js/script.js
@@ -10,6 +10,7 @@ import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
 import pageLoad from './modules/page-load';
 import rules from './modules/rules';
+import AccentTypographyBuild from './modules/accent-typography';
 
 // init modules
 mobileHeight();
@@ -25,3 +26,24 @@ rules();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
+
+const accentTypographyIntroTitle = new AccentTypographyBuild(`.intro__title`, {animationDelay: 500});
+const accentTypographyIntroDate = new AccentTypographyBuild(`.intro__date`, {animationDelay: 1000});
+
+
+setTimeout(() => {
+  accentTypographyIntroTitle.runAnimation();
+  accentTypographyIntroDate.runAnimation();
+}, 500);
+
+document.body.addEventListener(`screenChanged`, (evt) => {
+  if (evt.detail.screenName === `top`) {
+    accentTypographyIntroTitle.runAnimation();
+    accentTypographyIntroDate.runAnimation();
+  } else {
+    setTimeout(() => {
+      accentTypographyIntroTitle.destroyAnimation();
+      accentTypographyIntroDate.destroyAnimation();
+    }, evt.detail.screenName === `prizes` ? 1000 : 0);
+  }
+});
